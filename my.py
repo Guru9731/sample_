a895103beabb24694577a058636503ca8ad62eb9
Guru9731 <guruprasadsv05@gmail.com>
2025-10-15 12:51:16 +0530

    Update my.py

--- my.py
+++ my.py
@@ -2,8 +2,9 @@ for i range (1,20):
 	for j in range (1,10):
 		print(i*j == "")
 # <<<<<<< brach1
-# print("this is my new command") 		
-# #this is another new command 
+print("this is my new command") 		
+this is another new command 
+print("all is good")
 # =======
 # this is my new command
 # this is another new command 
