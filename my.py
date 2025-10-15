353c3a99fbbf39a851c490a8ae859440c78be2a9
UDHAYANITHI <104779093+Devil05426@users.noreply.github.com>
2025-10-15 11:58:22 +0530

    Update my.py

--- my.py
+++ my.py
@@ -1,6 +1,5 @@
 for i range (1,20):
 	for j in range (1,10):
 		print(i*j == "")
-	print()
-print("this is my new command")
-print("this is another new command") 
+this is my new command
+this is another new command 
